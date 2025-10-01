b9dd9daf00af6a7df4e16bfb6d0448b48a8845c2
shubhampatki777 <shubhampatki10@gmail.com>
2025-10-01 19:30:53 +0530

    Delete C_Programming directory

--- C_Programming/C_Assignment_0/9_area_of_rectangle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-	float base = 10, height = 5, area;
-	area = 0.5 * base * height;
-	
-	printf("Base = % .2f, Height = %.2f\n", base, height);
-	printf("Area of triangle = %.2f\n", area);
-	return 0;
-}
