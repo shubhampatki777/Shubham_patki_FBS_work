dd92fe22a268a7c8490241c310dcc1d6625b267b
shubhampatki777 <shubhampatki10@gmail.com>
2025-10-08 19:33:07 +0530

    Delete C_Module/C_Assignment_0 directory

--- C_Module/C_Assignment_0/6_square_and_cube.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-	int num = 5, square, cube;
-	square = num * num * num;
-	
-	printf("Number: %d\nSquare: %d\nCube: %d\n", num, square, cube);
-	return 0;
-}
--- C_Module/C_Assignment_0/7_convert_minutes.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-	int minutes = 130;
-	int hours = minutes / 60;
-	int rem = minutes % 60;
-	
-	printf("%d minutes = %d hours and %d minutes\n", minutes, hours, rem);
-	return 0;
-}
