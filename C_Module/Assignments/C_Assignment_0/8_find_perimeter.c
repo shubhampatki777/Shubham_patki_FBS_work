83fa2e28e1a8721f47a0dfbe10802695c3748d2f
shubhampatki777 <shubhampatki10@gmail.com>
2025-10-08 19:33:38 +0530

    Delete C_Module directory

--- C_Module/Assignments/C_Assignment_0/8_find_perimeter.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-	int length = 15, width = 10, perimeter;
-	perimeter = 2 * (length + width);
-	
-	printf("Perimeter of rectangle = %d\n", perimeter);
-	return 0;
-}
--- C_Module/C_Tests/Test_1/3_print_last_two_digit.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main() {
-	int num = 223410;
-	int r;
-	r = num % 100;
-	
-	printf("%d",r);
-}
