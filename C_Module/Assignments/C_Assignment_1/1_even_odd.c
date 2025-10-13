67c0fb2caacc7105f182af51bb5277367524b0fb
shubhampatki777 <shubhampatki10@gmail.com>
2025-10-13 15:11:28 +0530

    1_even_odd.c

--- C_Module/Assignments/C_Assignment_1/1_even_odd.c
+++ C_Module/Assignments/C_Assignment_1/1_even_odd.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
-void evenOdd(); //function declaration
-void main() {
-		evenOdd(); //function call
-		}
-	void evenOdd() { //function def
-		int num = 6;
-		
+int main() {
+	int num = 6;
 	if(num % 2 == 0)
 		printf("%d is Even\n", num);
 	else
 		printf("%d is Odd\n", num);
+	return 0;
+}
 	
-	}
-	
+
