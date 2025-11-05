2112e7e141c9474afb9ab1502138d18e8df344f2
shubhampatki777 <shubhampatki10@gmail.com>
2025-11-05 20:57:18 +0530

    Delete C_Module/Assignments directory

--- C_Module/Assignments/C_Assignment_0/3_convert_temp.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main() {
-	float celsius = 37, fahrenheit;
-	fahrenheit = (celsius * 9 / 5) + 32;
-	printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
-	return 0; 
-}
