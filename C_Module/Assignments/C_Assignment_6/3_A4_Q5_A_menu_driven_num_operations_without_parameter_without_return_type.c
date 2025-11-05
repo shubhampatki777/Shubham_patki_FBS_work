be13fa4c76542df1d2960221d9f739eefdf0106c
shubhampatki777 <shubhampatki10@gmail.com>
2025-11-05 21:24:13 +0530

    C_Assignment_6

---
+++ C_Module/Assignments/C_Assignment_6/3_A4_Q5_A_menu_driven_num_operations_without_parameter_without_return_type.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+
+void numberMenu();  // Function declaration
+
+void main() {
+    numberMenu();   // Function call
+}
+
+void numberMenu() {  // Function definition
+    int choice, num, temp, rev = 0, rem, sum = 0, flag = 0;
+
+    num = 123;  
+
+    printf("\nMenu:\n");
+    printf("1. Check Even or Odd\n");
+    printf("2. Check Prime or Not\n");
+    printf("3. Check Palindrome or Not\n");
+    printf("4. Check Positive, Negative or Zero\n");
+    printf("5. Reverse a Number\n");
+    printf("6. Find Sum of Digits\n");
+
+    choice = 3; 
+
+    switch(choice) {
+        case 1: 
+            if(num % 2 == 0) 
+                printf("%d is Even\n", num);
+            else 
+                printf("%d is Odd\n", num);
+            break;
+
+        case 2:
+            if(num <= 1)
+                printf("%d is Not Prime\n", num);
+            else {
+                flag = 0;
+                for(int i = 2; i <= num / 2; i++) {
+                    if(num % i == 0) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 0)
+                    printf("%d is Prime\n", num);
+                else 
+                    printf("%d is Not Prime\n", num);
+            }
+            break;
+
+        case 3:
+            temp = num;
+            rev = 0;
+            while(temp != 0) {
+                rem = temp % 10;
+                rev = rev * 10 + rem;
+                temp /= 10;
+            }
+            if(rev == num)
+                printf("%d is Palindrome\n", num);
+            else 
+                printf("%d is Not Palindrome\n", num);
+            break;
+
+        case 4:
+            if(num > 0)
+                printf("%d is Positive\n", num);
+            else if(num < 0)
+                printf("%d is Negative\n", num);
+            else 
+                printf("Number is Zero\n");
+            break;
+
+        case 5:
+            temp = num;
+            rev = 0;
+            while(temp != 0) {
+                rem = temp % 10;
+                rev = rev * 10 + rem;
+                temp /= 10;
+            }
+            printf("Reverse = %d\n", rev);
+            break;
+
+        case 6:
+            temp = num;
+            sum = 0;
+            while(temp != 0) {
+                rem = temp % 10;
+                sum += rem;
+                temp /= 10;
+            }
+            printf("Sum of digits = %d\n", sum);
+            break;
+
+        default:
+            printf("Invalid Choice!\n");
+    }
+}
