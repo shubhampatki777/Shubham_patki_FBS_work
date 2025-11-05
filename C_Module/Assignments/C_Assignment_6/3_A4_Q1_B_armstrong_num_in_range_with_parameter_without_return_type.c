110acba03567207c1ecff5fe9945fed12284f29d
shubhampatki777 <shubhampatki10@gmail.com>
2025-11-05 20:53:33 +0530

    C_Assignment_6

---
+++ C_Module/Assignments/C_Assignment_6/3_A4_Q1_B_armstrong_num_in_range_with_parameter_without_return_type.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+
+void printArmstrong(int n);  // Function declaration
+
+void main() {
+    printArmstrong(500);   // Function call with argument
+}
+
+void printArmstrong(int n) {  // Function definition
+    int num, temp, rem, digits, sum;
+
+    printf("Armstrong numbers from 1 to %d are:\n", n);
+
+    for(num = 1; num <= n; num++) {
+        temp = num;
+        sum = 0;
+        digits = 0;
+
+        int t = num;
+        while(t != 0) {
+            t /= 10;
+            digits++;
+        }
+
+        t = num;
+        while(t != 0) {
+            rem = t % 10;
+            sum += pow(rem, digits);
+            t /= 10;
+        }
+
+        if(sum == num)
+            printf("%d ", num);
+    }
+    printf("\n");
+}
---
+++ C_Module/Assignments/C_Assignment_6/3_A4_Q4_A_strong_num_in_range_without_parameter_without_return_type.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int fact(int n) { 
+    int i, f = 1;
+    for(i = 1; i <= n; i++)
+        f *= i;
+    return f;
+}
+
+void printStrong() {  
+    int n = 500, num, rem, sum, temp;
+
+    printf("Strong numbers from 1 to %d are:\n", n);
+
+    for(num = 1; num <= n; num++) {
+        temp = num;
+        sum = 0;
+        while(temp != 0) {
+            rem = temp % 10;
+            sum += fact(rem);
+            temp /= 10;
+        }
+        if(sum == num)
+            printf("%d ", num);
+    }
+    printf("\n");
+}
+
+void main() {
+    printStrong();  
+}
